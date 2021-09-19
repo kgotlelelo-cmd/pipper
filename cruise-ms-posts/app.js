17922ab85756e0dcd51f756cc48d993d4c1d6aba
kgotlelelo-cmd <kmasenam@student.wethinkcode.co.za>
2021-09-19 10:14:45 +0200

    the .env file for posts

--- cruise-ms-posts/app.js
+++ cruise-ms-posts/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express();
+require("dotenv").config();
 
 var bodyParser = require("body-parser");
 
@@ -9,7 +10,8 @@ app.use(express.urlencoded({ extended: false }));
 const indexRouter = require('./index');
 app.use('/',indexRouter);
 
-const port = 3001;
+const port = process.env.DEV_PORT;
+console.log(port)
 
 app.listen(port,()=>{
     console.log(`running on port ${port}`)
