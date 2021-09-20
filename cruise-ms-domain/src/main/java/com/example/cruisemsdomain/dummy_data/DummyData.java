04076afd70332dde8b46b4c744927c2887f57724
SirNoob97 <martinald467@gmail.com>
2021-09-20 21:03:19 +0200

    refactor - builder pattern

--- cruise-ms-domain/src/main/java/com/example/cruisemsdomain/dummy_data/DummyData.java
+++ cruise-ms-domain/src/main/java/com/example/cruisemsdomain/dummy_data/DummyData.java
@@ -2,7 +2,7 @@ package com.example.cruisemsdomain.dummy_data;
 
 import com.example.cruisemsdomain.entity.Client;
 import com.example.cruisemsdomain.model.Gender;
-import com.example.cruisemsdomain.model.Post;
+import com.example.cruisemsdomain.entity.Post;
 import com.example.cruisemsdomain.repository.ClientRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -13,34 +13,33 @@ import java.util.List;
 
 @Component
 public class DummyData {
-    @Bean
-    CommandLineRunner runner(ClientRepository repository){
-        return args -> {
-            Client dummyClient = new Client(
-                    "code bender",
-                    "kgotlelelo",
-                    "masenamela",
-                    "Have a lovely day",
-                    "kmasenam@student.wethinkcode.co.za",
-                    Gender.MALE,
-                    LocalDateTime.now(),
-                    List.of(new Post(
-                            "hello world",
-                            0
-                    ))
-            );
 
-            Client dummy1Client = new Client(
-                    "Don",
-                    "John",
-                    "Doe",
-                    "another one",
-                    "kgotlelelomasenamela74@gmail.com",
-                    Gender.FEMALE,
-                    LocalDateTime.now()
-            );
+	@Bean
+	CommandLineRunner runner(ClientRepository repository){
+		return args -> {
+			var dummyClient = Client.builder()
+				.username("code bender")
+				.firstName("kgotlelelo")
+				.lastName("masenamela")
+				.bio("Have a lovely day")
+				.email("kmasenam@student.wethinkcode.co.za")
+				.gender(Gender.MALE)
+				.dateOfBirth(LocalDateTime.now())
+				.posts(List.of(new Post("hello world", 0), new Post("Second", 1)))
+				.build();
 
-            repository.insert(List.of(dummyClient,dummy1Client));
-        };
-    }
+
+			var dummyClient2 = Client.builder()
+				.username("Don")
+				.firstName("John")
+				.lastName("Doe")
+				.bio("another one")
+				.email("kgotlelelomasenamela74@gmail.com")
+				.gender(Gender.MALE)
+				.dateOfBirth(LocalDateTime.now())
+				.build();
+
+			repository.insert(List.of(dummyClient,dummyClient2));
+		};
+	}
 }
