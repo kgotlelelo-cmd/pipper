30ac79890a05904dbed069d7a6c74df0f1cd3cc4
SirNoob97 <martinald467@gmail.com>
2021-09-21 21:29:07 +0200

    unnecessary

--- cruise-ms-domain/src/test/java/com/example/cruisemsdomain/CruiseMsDomainApplicationTests.java
+++ cruise-ms-domain/src/test/java/com/example/cruisemsdomain/CruiseMsDomainApplicationTests.java
@@ -1,7 +1,5 @@
 package com.example.cruisemsdomain;
 
-import com.example.cruisemsdomain.config.TestContainer;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
