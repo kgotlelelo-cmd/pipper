fa36686a1cde68bf6641f5ec5dffb6dd2e83fa54
SirNoob97 <martinald467@gmail.com>
2021-09-21 18:56:45 +0200

    fix id type

--- cruise-ms-domain/src/main/java/com/example/cruisemsdomain/repository/PostRepository.java
+++ cruise-ms-domain/src/main/java/com/example/cruisemsdomain/repository/PostRepository.java
@@ -4,5 +4,5 @@ import com.example.cruisemsdomain.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface PostRepository extends JpaRepository<Post, Integer> {
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
