7b3455dc414e71fdab5b02b4af09948a9424fcdb
kgotlelelo-cmd <kmasenam@student.wethinkcode.co.za>
2021-09-27 20:59:20 +0200

    find user by email still need fixing

--- cruise-ms-registration/src/services/userInfo.js
+++ cruise-ms-registration/src/services/userInfo.js
@@ -12,4 +12,15 @@ function fetchSpecificUser(callback,url=process.env.DEV_URL_CLIENT_BY_ID){
         });
 }
 
-module.exports = fetchSpecificUser;
+function fectchUserByEmail(callback,url=process.env.DEV_URL_CLIENT_BY_EMAIL){
+    logger.info("fetching a specific clients by email from domain application");
+    fetch(url)
+        .then(res=> res.json())
+        .then(callback)
+        .then(json => logger.debug(json))
+        .catch((err)=>{
+            logger.error(err);
+        });
+}
+
+module.exports = fetchSpecificUser,fectchUserByEmail;
